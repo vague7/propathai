c58f42656c1745e790ea93daccfd786d68c2b792
Amarendra Das <amarendra.das999@gmail.com>
2025-06-10 18:40:41 +0530

    home page fixed

--- components/hero.jsx
+++ components/hero.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
+              src="/banner.jpg"
               width={1280}
               height={720}
               alt="Dashboard Preview"
